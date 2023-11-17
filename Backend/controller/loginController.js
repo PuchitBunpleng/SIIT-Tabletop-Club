ff8c36ecb0bfa26239507221a53aa89f72bb7318
Puchit Bunpleng <6422780104@g.siit.tu.ac.th>
2023-11-17 23:46:27 +0700

    Update loginController.js

--- Backend/controller/loginController.js
+++ Backend/controller/loginController.js
@@ -1,5 +1,7 @@
 let post = (req, res) => {
-    console.log(req.session)
+    // query to get user id and then
+    // req.session.userID = {userID}
+    // where {userID} is obtained from the query result.
 }
 
 export default { post }
