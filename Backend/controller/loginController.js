e7e55a2ad197f1a245ef87d7754209f6fb001cd6
Patharapol1573 <149755180+Patharapol1573@users.noreply.github.com>
2023-11-18 00:58:08 +0700

    create loginController
    create loginController

--- Backend/controller/loginController.js
+++ Backend/controller/loginController.js
@@ -1,7 +1,24 @@
-let post = (req, res) => {
-    // query to get user id and then
-    // req.session.userID = {userID}
-    // where {userID} is obtained from the query result.
+import memberModel from "../model/memberModel.js"
+
+
+let post = async (req, res) => {
+    try {
+        const result = await memberModel.getByID(req.body.std_id)
+        var password = req.body.password
+        const inputpass = result[0].password
+        if (password === inputpass) {
+            const userID = result[0].std_id
+            const core = result[0].core
+            req.session.userID = userID
+            req.session.core = core
+            return res.sendStatus(200)
+        }
+        else {
+            return res.status(403).json({message: "Incorrect password"})
+        }
+    } catch (err) {
+        return res.status(500).json(err.message)
+    }
 }
 
 export default { post }
