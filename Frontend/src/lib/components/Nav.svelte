dbb4d961d123a50c008c6f524b362687425900ac
Puchit Bunpleng <6422780104@g.siit.tu.ac.th>
2023-11-20 21:48:42 +0700

    Update Nav.svelte

--- Frontend/src/lib/components/Nav.svelte
+++ Frontend/src/lib/components/Nav.svelte
@@ -16,7 +16,7 @@
 		if (id) {
 			try {
 				const response = await api.get(`/member/${id}`);
-				core = response.data[0].core || 0;
+				core = response?.data[0]?.core || 0;
 			} catch (error) {
 				console.log(error);
 			}
