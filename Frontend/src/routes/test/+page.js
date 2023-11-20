f9bfcc1ac68aac7d2c010f6ce817418399b25b18
Puchit Bunpleng <6422780104@g.siit.tu.ac.th>
2023-11-20 20:06:11 +0700

    add test

---
+++ Frontend/src/routes/test/+page.js
@@ -0,0 +1,14 @@
+import { error } from '@sveltejs/kit'
+import { userID } from '$lib/store.js'
+import api from '$lib/api.js'
+
+export async function load() {
+  try {
+    let id = null
+    userID.subscribe(val => { id = val })
+    let data = (await api.get(`/member/${id}`)).data[0]
+    return data
+  } catch (err) {
+    throw error(500, err);
+  }
+}
